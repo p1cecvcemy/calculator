b2666a451bee566524c5c46d999f387993e4acda
roman <p1cecvcemy07@mail.ru>
2024-10-12 15:45:59 +0300

    Добавил Taildwind css

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'tailwindcss/tailwind.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Привет мир</h1>
+        <div className="flex justify-center items-center">
+          <button className="bg-blue-500 px-4 py-2 rounded">Click Me</button>
+        </div>
       </header>
     </div>
   );
